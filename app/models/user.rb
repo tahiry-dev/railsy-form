397cd5d75a004048768bbd11b5e1ba12b524c987
tahiry-dev <tahirycontact@gmail.com>
2020-11-26 16:56:32 +0300

    implement a form with html

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ApplicationRecord
     validates :username, :email, :password, presence: true, uniqueness: true
-    validates :password, length: { minumum: 6}
+    validates :password, length: { minumum: 6, maximum: 15}
 end
