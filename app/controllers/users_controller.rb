32cfbbb2c2bc9b1106036531c7de1669f375f618
tahiry-dev <tahirycontact@gmail.com>
2020-11-27 17:29:30 +0300

    add form token to the simple html

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,7 +17,7 @@ class UsersController < ActionController::Base
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to users_path
+      redirect_to new_user_path
     else
       render :new
     end
